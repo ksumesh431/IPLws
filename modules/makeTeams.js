55f533ce65ae717a0cb828b110fa394678e49db4
ksumesh431 <juuzou431@gmail.com>
2021-04-04 19:14:43 +0530

    makeTeams modifed

--- modules/makeTeams.js
+++ modules/makeTeams.js
@@ -1,20 +1,28 @@
-const{fs,path,request,cheerio,createDir}=require("../includes");
+const { fs, path, request, cheerio, createDir } = require("../includes");
 
-function makeTeams(url){
-    request(url,(err,res,html)=>{
-        if(err){
+function makeTeams(url) {
+    request(url, (err, res, html) => {
+        if (err) {
             console.log(err);
-        }else{
-            let selectorTool = cheerio.load(html);
-            let teams = selectorTool(".table.table-sm.standings-widget-table.text-center.mb-0.border-bottom tbody tr td:first-child a")
-            for (let index = 0; index < teams.length; index++) {
-                const element = selectorTool(teams[index]);
-                let team = (element.text().substring(1));
-                createDir(path.join(__dirname+"\\..\\IPL 2020\\"+team))
+        } else {
+            let selTool = cheerio.load(html);
+            let namesArr = selTool("tbody tr .header-title.label");
+            for (let i = 0; i < namesArr.length; i++) {
+                let teamName = selTool(namesArr[i]).text();
+                // console.log(teamName+"\n");
+                createDir(path.join(__dirname + "\\..\\IPL 2020\\" + teamName));
             }
+
         }
     })
 }
-module.exports={
+module.exports = {
     makeTeams
 }
+// let selectorTool = cheerio.load(html);
+// let teams = selectorTool(".table.table-sm.standings-widget-table.text-center.mb-0.border-bottom tbody tr td:first-child a")
+// for (let index = 0; index < teams.length; index++) {
+//     const element = selectorTool(teams[index]);
+//     let team = (element.text().substring(1));
+//     createDir(path.join(__dirname + "\\..\\IPL 2020\\" + team))
+// }
